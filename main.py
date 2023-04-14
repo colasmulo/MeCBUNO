2c429c409685d45c77f44f8f6d594681e86d780d
Gabriel Poirier <gabriel.poirier@orange.fr>
2023-04-14 23:44:50 +0200

    variables in ddb file

--- main.py
+++ main.py
@@ -7,17 +7,8 @@ Created on Thu Jul 28 12:33:00 2022
 """
 import tkinter as tk
 from tkinter import ttk
-from planeur_ddb import 
-
-#Variables, listes
-T77 = [398, -1.140, -0.011, 0.685] #[Poids à vide (kg), bras de levier pilote avant(m), bras de levier pilote arrière(mm), bras de levier CG vide]
-T41 = [409, -1.140, -0.011, 0.71265]
-JI = [409, -1.350, -0.270, 0.783]
-LK = [412.8, -1.350, -0.270, 0.803]
-PAP = [310, -5, 10, 0.7]#Valeurs par défaut à corriger
-C4 = [310, -5, 10, 0.7]#Valeurs par défaut à corriger
-Planeur_list = [T77, T41, JI, LK, PAP, C4]
-Planeur_list_str = ["T77", "T41", "JI", "LK", "PAP", "C4"]
+from planeur_ddb import Planeur_list
+from planeur_ddb import Planeur_list_str
 
 def Calculer():
     P1 = int(Pil_av.get())
---
+++ planeur_ddb.py
@@ -0,0 +1,9 @@
+#Variables, listes
+T77 = [398, -1.140, -0.011, 0.685] #[Poids à vide (kg), bras de levier pilote avant(m), bras de levier pilote arrière(mm), bras de levier CG vide]
+T41 = [409, -1.140, -0.011, 0.71265]
+JI = [409, -1.350, -0.270, 0.783]
+LK = [412.8, -1.350, -0.270, 0.803]
+PAP = [310, -5, 10, 0.7]#Valeurs par défaut à corriger
+C4 = [310, -5, 10, 0.7]#Valeurs par défaut à corriger
+Planeur_list = [T77, T41, JI, LK, PAP, C4]
+Planeur_list_str = ["T77", "T41", "JI", "LK", "PAP", "C4"]
