066c2b1d6394bd2de90d1882849ff39a7b8d5b41
Gabriel Poirier <gabriel.poirier@orange.fr>
2023-04-14 23:51:17 +0200

    working ddb file with import

--- planeur_ddb.py
+++ planeur_ddb.py
@@ -1,5 +1,6 @@
 #Variables, listes
-T77 = [398, -1.140, -0.011, 0.685] #[Poids à vide (kg), bras de levier pilote avant(m), bras de levier pilote arrière(mm), bras de levier CG vide]
+#[Poids à vide (kg), bras de levier pilote avant(m), bras de levier pilote arrière(mm), bras de levier CG vide]
+T77 = [398, -1.140, -0.011, 0.685]
 T41 = [409, -1.140, -0.011, 0.71265]
 JI = [409, -1.350, -0.270, 0.783]
 LK = [412.8, -1.350, -0.270, 0.803]
