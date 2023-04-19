1ff0302001d6e9a89547730f5d2ea80e71987389
Gabriel Poirier <gabriel.poirier@orange.fr>
2023-04-19 15:47:24 +0200

    get club from user selection

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ Created on Thu Jul 28 12:33:00 2022
 """
 import tkinter as tk
 from tkinter import ttk
-from planeur_ddb import Planeur_list, Planeur_list_str
+#from planeur_ddb import Planeur_list, Planeur_list_str
+
+Clubs = ["BUNO", "ORLEANS"]
 
 def Calculer():
     P1 = int(Pil_av.get())
@@ -17,33 +19,39 @@ def Calculer():
     Masse = Planeur_list[pl_index][0] + P1 + P2
     Moment = Planeur_list[pl_index][1]*P1 + Planeur_list[pl_index][2]*P2 + Planeur_list[pl_index][3]*Planeur_list[pl_index][0]
     CG = round((Moment/Masse)*1000, 2)
-    tk.Label(frame, text="Planeur : %s\nPosition du centre de gravité (en mm): %s" % (Planeur_input, CG)).grid(row=4)
+    tk.Label(frame, text="Planeur : %s\nPosition du centre de gravité (en mm): %s" % (Planeur_input, CG)).grid(row=5)
 
 # Top level window
 frame = tk.Tk()
 frame.title("Masse et Centrage BUNO")
+#Choix planeur
+tk.Label(frame, text="Selectionnez le club :").grid(row=0)
+Club_deroul = ttk.Combobox(frame, values=Clubs)
+Club_deroul.grid(row=0, column=1)
+Club_sel = Club_deroul.get()
+from Club_sel import Planeur_list, Planeur_list_str
 #Immat planeur
-tk.Label(frame, text="Selectionnez l'immat concours du planeur :").grid(row=0)
+tk.Label(frame, text="Selectionnez l'immat concours du planeur :").grid(row=1)
 Immat_deroul = ttk.Combobox(frame, values=Planeur_list_str)
-Immat_deroul.grid(row=0, column=1)
+Immat_deroul.grid(row=1, column=1)
 #Poids pil avant
-tk.Label(frame, text="Masse du pilote avant en kilos :").grid(row=1)
+tk.Label(frame, text="Masse du pilote avant en kilos :").grid(row=2)
 Pil_av = tk.Entry(frame)
-Pil_av.grid(row=1, column=1)
+Pil_av.grid(row=2, column=1)
 #Poids pil arriere
-tk.Label(frame, text="Masse du pilote arrière en kilos (0 si  pas de pilote arrière):").grid(row=2)
+tk.Label(frame, text="Masse du pilote arrière en kilos (0 si  pas de pilote arrière):").grid(row=3)
 Pil_ar = tk.Entry(frame)
-Pil_ar.grid(row=2, column=1)
+Pil_ar.grid(row=3, column=1)
 
 tk.Button(frame,
             text='Quitter',
-            command=frame.quit).grid(row=3,
+            command=frame.quit).grid(row=4,
                                     column=0,
                                     sticky=tk.W,
                                     pady=4)
 tk.Button(frame,
             text='Calculer',
-            command=Calculer).grid(row=3,
+            command=Calculer).grid(row=4,
                                     column=1,
                                     sticky=tk.W,
                                     pady=4)
